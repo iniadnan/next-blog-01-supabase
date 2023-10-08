f1cddc1579ce69d12414eac2fb251c1df2c51204
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-10-08 17:54:58 +0700

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <nav className="w-full border-b-2 py-3 bg-gray-50">
+            <div className="container mx-auto px-5 flex items-center justify-between">
+                <div className='flex items-end gap-x-5'>
+                    <Link href='/' className="font-semibold text-xl md:text-3xl text-gray-700 inline-block mr-5">Home</Link>
+                    <Link href='/about' className="font-medium text-sm md:text-base text-gray-600">About</Link>
+                    <Link href='/contact' className="font-medium text-sm md:text-base text-gray-600">Contact</Link>
+                </div>
+                <button type="button" className="text-base text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium py-2 px-5 rounded-lg">New Post</button>
+            </div>
+        </nav>
+    )
+}
